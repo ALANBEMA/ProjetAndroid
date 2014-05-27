cacf96b178dd615379de1134ba8cecbd064c0e20
alexbzh <alexandre.bzh@gmail.com>
2014-05-27 11:14:07 +0200

    Création BDD.java

---
+++ src/fr/epsi/ALANBEMA/BDD.java
@@ -0,0 +1,43 @@
+package fr.epsi.ALANBEMA;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by alexandredouchin on 26/05/2014.
+ */
+public class BDD {
+    BDDHelper helper;
+
+    SQLiteDatabase base;
+
+    public void open(Context activity) {
+        //Créer ou ouvrir la BDD
+        helper = new BDDHelper(activity);
+        base = helper.getWritableDatabase();
+    }
+
+    public Cursor getInfosPersonnel() {
+        return base.rawQuery(
+                "SELECT _id, nom, prenom, adresse, email, numTel FROM personnel", null
+        );
+    }
+
+    public Cursor getInfosAgenda() {
+        return base.rawQuery(
+                "SELECT _id, id_personnel, date_debut, date_fin", null
+        );
+    }
+
+    public void addPersonnel(personnel personnel) {
+        String requete = "INSERT INTO personnel (nom, prenom, adresse, email, numTel) VALUES ( '" +
+                "" + personnel.getNom() + "', '" + personnel.getPrenom() + "', '" + personnel.getAdresse() + "', '" + personnel.getEmail() + "', '" + personnel.getNumTelephone() + "');";
+        base.execSQL(requete);
+    }
+
+    public void addAgenda(agenda agenda) {
+        String requete = "INSERT INTO agenda (id_personnel, date_debut, date_fin) VALUES ( '" + agenda.getId_personnel() + "', '" + agenda.getDate_debut() + "', '" + agenda.getDate_fin() + "');";
+        base.execSQL(requete);
+    }
+}
